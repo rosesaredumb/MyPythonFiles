802d52ce17e0503c887cc35535ec74ed7070c3a8
rosesaredumb <70898141+rosesaredumb@users.noreply.github.com>
2024-10-23 03:42:32 +0530

    c

--- globals.py
+++ globals.py
@@ -1,8 +1,6 @@
 import asyncio
 from datetime import datetime
-import discord
-from discord import app_commands
-from discord.ext import commands
+
 from functools import partial
 import json
 import logging
