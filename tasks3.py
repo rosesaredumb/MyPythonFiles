b199085e9806768a058fdd7cd42c74f2023e680e
rosesaredumb <70898141+rosesaredumb@users.noreply.github.com>
2024-10-23 17:28:45 +0000

    C

--- tasks3.py
+++ tasks3.py
@@ -16,39 +16,80 @@ class MyTasks:
     def add_task(self):
         task_description = ""
         while not task_description.strip():  # Loop until non-empty input is provided
-            task_description = str(input("Enter task description: ")).lower()
+            task_description = str(input("Enter task description: "))
         data = self.json_helper.read_json(self.filepath)
 
-        if len(data["tasks"]) > 0:
-            categories_list = self.view_categories()
-            if len(categories_list) > 0:
-                print("Choose a category:")
-                for idx, category in enumerate(categories_list, 1):
-                    print(f"{idx} - {category}")
+        
+        categories_list = self.view_categories()
+        chosen_category = None
+        if len(categories_list) > 0:
+            print("Choose a category:")
+            for idx, category in enumerate(categories_list, 1):
+                print(f"{idx} - {category}")
+            
+            # Prompt the user to enter a number
+            choice = input("Enter a number to choose the category: ")
+            if choice:
+                try:
+                    choice = int(choice)
+                    if 1 <= choice <= len(categories_list):
+                        chosen_category = categories_list[choice - 1]
+                        print(f"You selected: {chosen_category}")
+                        return chosen_category
+                    elif choice == 0:
+                        chosen_category = ""
+                        while not chosen_category.strip():
+                            chosen_category = str(input("Enter name of the category you want to create: ")).lower()
+                        if chosen_category not in categories_list:
+                            print(f"Category: {chosen_category} created!")
+                            return chosen_category
+                        else:
+                            print("Category already exists!")
+                            return chosen_category
+                    elif choice > len(categories_list):
+                        print("Invalid choice! Please try again.")
+                        return None
+        
+                except ValueError: 
+                    print("Please enter a valid number.")
 
-                # Prompt the user to enter a number
-                choice = int(input("Enter a number to choose the category: "))
-
-                # Ensure the input is valid
-                if 1 <= choice <= len(categories_list):
-                    chosen_category = categories_list[choice - 1]
-                    print(f"You selected: {chosen_category}")
-                else:
-                    print("Invalid choice")
+        else:
+            choice = input("no existing categories. Enter a 0 to make the category or click enter: ")
+            if choice:
+                try:
+                    choice = int(choice)
+                    if choice == 0:
+                        chosen_category = ""
+                        while not chosen_category.strip():
+                            chosen_category = str(input("Enter name of the category you want to create: ")).lower()
+                    else:
+                        print("Invalid choice! Please try again.")
+                        return None
+                except ValueError: 
+                    print("Please enter a valid number.")
+            
         data["tasks"].append({
             "description": task_description, 
-            "status": False
+            "status": False,
+            "category": chosen_category
         })
+        self.json_helper.write_json(data, self.filepath)
+        print("done")
+            
+    
+        
 
 
     def view_categories(self):
-        try:
-            data = self.json_helper.read_json(self.filepath)
-            #if len(data["tasks"]) > 0:
+        data = self.json_helper.read_json(self.filepath)
+        
+        if len(data["tasks"]) > 0:
             categories_list = list({task["category"] for task in data["tasks"] if task["category"]})
             # Convert the set to a list (to remove duplicates) and print the result
             print("Categories:", categories_list)
             return categories_list
+        else:
+            return []
         
         
 
